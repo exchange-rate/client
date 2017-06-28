263d494de90bb9bf178bf27a793b364bb3cfbacf
vtrushin <monospectr@gmail.com>
2017-06-28 03:13:18 +0300

    Changed styles

--- src/popup.js
+++ src/popup.js
@@ -3,7 +3,21 @@ import ReactDOM from 'react-dom';
 import Popup from './components/Popup';
 import './styles/main.sass';
 
-ReactDOM.render(
-	<Popup/>,
-	document.getElementById('app')
-);
+const appEl = document.getElementById('app');
+
+document.body.style.opacity = 0;
+document.body.style.transition = 'opacity ease-out .4s';
+
+// Work around https://bugs.chromium.org/p/chromium/issues/detail?id=428044
+function onReady() {
+	requestAnimationFrame(() => {
+		document.body.style.opacity = 1;
+		document.body.style.width = '100px';
+		document.body.style.width = '';
+	});
+}
+
+document.addEventListener('DOMContentLoaded', onReady);
+window.onload = onReady;
+
+ReactDOM.render(<Popup/>, appEl, onReady);
